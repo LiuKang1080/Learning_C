1d86a6b1582a72d81360fcf7095e50fb223c3b18
Shivakumar Mahakali <mahakalim@yahoo.com>
2019-06-16 18:09:19 -0400

    added and changed some comments

--- Basics_Of_C/Sources/Hello_World.c
+++ Basics_Of_C/Sources/Hello_World.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 // comments in C are done with // 
-// basic hello world in C 
+// multi-line comments are started with ( /* ) and end with ( */ ) 
 
+// basic hello world in C 
 int main() {
     printf("Hello, my name is Shiva!");
     return 0;
 }
 
-// Basic structure of a C program:
-// All C programs contain one main() function. main() is the entry point into the program. 
-// Case sensitivity matters in C. 
-// All statements end with a semi-colon ;
-// main is a reserved keyword, it cannot be declared as a variable. 
+/* 
+Basic structure of a C program:
+All C programs contain one main() function. main() is the entry point into the program. 
+Case sensitivity matters in C. 
+All statements end with a semi-colon ;
+main is a reserved keyword, it cannot be declared as a variable. 
+*/
