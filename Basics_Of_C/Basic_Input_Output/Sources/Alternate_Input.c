8341e8512712d35a18b08cce8c0c02fe1f52d2e6
Shivakumar Mahakali <mahakalim@yahoo.com>
2019-06-18 03:21:07 -0400

    Added Alternate_Input.c file

---
+++ Basics_Of_C/Basic_Input_Output/Sources/Alternate_Input.c
@@ -0,0 +1,14 @@
+// Alternative form for reading inputs from keyboard
+#include <stdio.h>
+
+int main() {
+    //Create a variable called name:
+    char name[20];
+    printf("Hello, What is your name? \n");
+
+    // Here we use the alternative form for input the fgets() function.
+    fgets(name, 20, stdin);
+    printf("Hello there, %s", name);
+
+    return 0;
+}
