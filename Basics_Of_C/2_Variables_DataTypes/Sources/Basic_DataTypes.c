380a25815a09c01295009bc6defa63c20bab2253
Shivakumar Mahakali <mahakalim@yahoo.com>
2019-06-20 02:59:59 -0400

    Added Basic_DataTypes.C

---
+++ Basics_Of_C/2_Variables_DataTypes/Sources/Basic_DataTypes.c
@@ -0,0 +1,36 @@
+// Basic Data Types in C
+
+#include <stdio.h>
+
+int main() {
+    /*
+    Basic Data Types in C:
+        - int
+        - float
+        - double
+        - char 
+        - _bool 
+    
+    int Data Type:
+        - Contains whole numbers only, no decimals.
+        - A minus sign indicates negative values.
+        - int data type is a signed integer, it must be an integer (positive, negative, or zero).
+        - if an int is preceded by an 0 followed by an x then it is expressed as an hexadecimal value.
+        
+        int rgbColor = 0xFFEF0D; 
+
+        - No embedded spaces
+        - Commas are not allowed to represent values greater than 999
+
+    float Data Type:
+        - Stores floating point numbers (values containing decimal places)
+        - floats can be expressed in scientific notation  
+
+        float my_val = 2.69e7;
+        
+
+    */
+    
+
+    return 0;
+}
