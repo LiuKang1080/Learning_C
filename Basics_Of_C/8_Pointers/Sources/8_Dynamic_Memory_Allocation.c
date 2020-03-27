5c709a614d288103e2a3b75a6544cbf2bb371afc
Shivakumar Mahakali <mahakalim@yahoo.com>
2020-03-26 21:36:14 -0400

    Added more comments to 8_Dynamic_Memory_Allocation.c

--- Basics_Of_C/8_Pointers/Sources/8_Dynamic_Memory_Allocation.c
+++ Basics_Of_C/8_Pointers/Sources/8_Dynamic_Memory_Allocation.c
@@ -16,11 +16,73 @@
 
     Heap vs Stack
         - Dynamic memory allocation reserves space in a memory area called the heap.
+        - The Stack: Another location where memory is allocated. Function arguments and local variables in a function are stored 
+            here. When the execution of a function ends the space allocated to store the arugments and local variables are freed.
+        - When a function is called the function and the local variables it will use will get placed into the stack. Once the 
+            function is done running the data related to the function and it's local variables will be freed. This is one of the
+            reasons why when we dynamically allocate memory it goes to the heap instead of the stack, data lasts longer in the heap
+            compared to the stack.
+        - We get to control when things enter the heap and when it gets deleted. We have NO control over what / when data enters
+            and gets deleted in the stack, the compiler will automatically do that for us.
+
+    Dynamic Memory Allocation Functions
+        - There are 3 major function that we can use in C to dynamically allocate memeory: malloc, calloc, and realloc
+        - In order to use these function we need to include the stdlib.h header file.
+        
+        1) malloc()
+            - This is the simplest memory function. It will allocate the memory at run-time. 
+            - We specify the number of bytes of memory we want to allocate as the argument.
+            - This function returns the address of the first byte of memory that is allocated.
+            - Since the function returns an address a pointer is the only variable that can store it.
+
+            General Syntax:
+            int *p_number = (int *)malloc(100);
+
+            - We request 100 bytes of memory to be allocated and assigned the memory block to p_number 
+            - p_number will point to the first int location at the beginning of the 100 bytes that was allocated
+            
+            - There is an issue using malloc() in this manner. In this example int can hold 4 bytes each, totaling 25 ints.
+            - This code may or may not work on ALL systems, because the size of an int can be different on different systems  
+
+            - We need to remove the assumption that all systems will have an int size of 4 bytes. We can do this by using the 
+                sizeof() operator.
+            
+            General Syntax:
+            int *p_number = (int *)malloc(25 * sizeof(int));
+
+            - Here we are saying to allocate 25 blocks of int whatever the size of an int is.
+            - The cast (int *) converts the address returned by the function to the type "pointer" to an "int"
+            - malloc() returns a pointer of type void, so we need to perform this cast.
+            
+            - We can request any number of bytes.
+            - If the memory amount we request can not be allocated for any reason then malloc() will return a NULL
+            - It is ALWAYS a good idea to check any dynamic memory allocation request immediately by using an if statement
+                to make sure the memory is actually there before we can use it
+
+            int *p_number = (int *)malloc(2500000 * sizeof(int));
+            if(!p_number) {
+                // code to deal with memory allocation failure;
+            }
+
+            - This is important that we perform these checks. We do NOT want the program to continue running if we can't properly
+                allocate the memory, this will result in a crash!
+            
+        Releasing Memory
+            - When we allocate memory dynmically, we should ALWAYS release the memory when it is no longer required
+            - Memory that we allocate to the heap will automatically be released when the program ends, but we should manually
+                free the memory even if its right before the program end
+            - The best programming practice is to ALWAYS manually release the memory when we're done using it.
+
+            - Memory Leaks = 
+
+
 
 */
 
 
 #include <stdio.h>
+// stdlib.h for the malloc(), calloc(), and realloc() functions 
+#include <stdlib.h>
 
 
 int main() {
